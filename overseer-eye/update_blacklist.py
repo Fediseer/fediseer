4903a6fa7b1cca07e0c3540b181de2e05990a1ad
Jason Grim <jason@gr.im>
2023-06-20 21:08:30 -0400

    Updated overseer-eye to have the overseer domain set via environmental variable

--- overseer-eye/update_blacklist.py
+++ overseer-eye/update_blacklist.py
@@ -2,8 +2,8 @@ import requests
 import os
 from pythorhead import Lemmy
 
-# Your own instance's domain
 LEMMY_DOMAIN = os.getenv('LEMMY_DOMAIN')
+OVERSEER_DOMAIN = os.getenv('OVERSEER_DOMAIN', 'overseer.dbzer0.com')
 USERNAME = os.getenv('USERNAME')
 PASSWORD = os.getenv('PASSWORD')
 # If there's this many registered users per local post+comments, this site will be considered suspicious
@@ -18,7 +18,7 @@ if lemmy.log_in(USERNAME, PASSWORD) is False:
     raise Exception("Could not log in to lemmy")
 
 print("Fetching suspicions")
-sus = requests.get(f"https://overseer.dbzer0.com/api/v1/instances?activity_suspicion={ACTIVITY_SUSPICION}&domains=true", timeout=5).json()
+sus = requests.get(f"https://{OVERSEER_DOMAIN}/api/v1/instances?activity_suspicion={ACTIVITY_SUSPICION}&domains=true", timeout=5).json()
 defed = blacklist | set(sus("domains"))
 print("Editing Defederation list")
 ret = lemmy.site.edit(blocked_instances=list(defed))
