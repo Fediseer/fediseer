e852a521beb26c616bccde1c3426288992adcaeb
kthouky <konstantinos.thoukydidis@ses.com>
2023-08-08 13:06:19 +0200

    fix

--- fediseer/database/functions.py
+++ fediseer/database/functions.py
@@ -67,9 +67,9 @@ def get_all_censured_instances_by_censuring_id(censuring_ids):
     query = db.session.query(
         Instance
     ).outerjoin(
-        Instance.censures_given,
+        Instance.censures_received,
     ).options(
-        joinedload(Instance.censures_given),
+        joinedload(Instance.censures_received),
     ).filter(
         Censure.censuring_id.in_(censuring_ids)
     ).group_by(
@@ -81,9 +81,9 @@ def get_all_censuring_instances_by_censured_id(censured_id):
     query = db.session.query(
         Instance
     ).outerjoin(
-        Instance.censures_received,
+        Instance.censures_given,
     ).options(
-        joinedload(Instance.censures_received),
+        joinedload(Instance.censures_given),
     ).filter(
         Censure.censured_id == censured_id
     ).group_by(
