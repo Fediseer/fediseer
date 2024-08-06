ae7b8acdd62dc00350cdd03c42e8ca48b4452847
db0 <mail@dbzer0.com>
2024-08-06 14:07:26 +0200

    test

--- fediseer/apis/v1/base.py
+++ fediseer/apis/v1/base.py
@@ -20,6 +20,7 @@ api_root = Api()
 api_root.representations["application/activity+json"] = output_json
 api_root.init_app(app, add_specs=False)
 api = Namespace('v1', 'API Version 1' )
+api_root.add_namespace(api)
 
 from fediseer.apis.models.v1 import Models
 
