b92712b77c7668d01966021fa7e1efd0ea792218
db0 <mail@dbzer0.com>
2025-08-25 15:54:50 +0200

    test

--- fediseer/fediverse.py
+++ fediseer/fediverse.py
@@ -128,11 +128,9 @@ class InstanceInfo():
         try:
             resolver = dns.resolver.Resolver()
             txt_records = resolver.resolve(self.domain, 'TXT')
-            logger.info(txt_records)
-            logger.info([record.to_text() for record in txt_records])
-            for record in txt_records:
-                logger.debug(record.to_text())
-                if record.to_text().strip('"').startswith('fediseer-admins='):
+            for record in [record.to_text() for record in txt_records]:
+                logger.debug(record)
+                if record.strip('"').startswith('fediseer-admins='):
                     admins = record.strip('"').split("=",1)[1].split(",")
                     logger.info(f"Found admins from TXT record: {admins}")
                     self.admin_usernames.update(admins)
