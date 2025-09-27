c4c25cae18d51a327a35fc134759b660f9cdc175
db0 <mail@dbzer0.com>
2025-09-27 17:38:06 +0200

    fix: sqlalchemy 2.x

--- fediseer/flask.py
+++ fediseer/flask.py
@@ -6,28 +6,32 @@ from flask_sqlalchemy import SQLAlchemy
 from loguru import logger
 
 cache = None
-OVERSEER = Flask(__name__)
-OVERSEER.wsgi_app = ProxyFix(OVERSEER.wsgi_app, x_for=1)
-
 SQLITE_MODE = os.getenv("USE_SQLITE", "0") == "1"
 
-if SQLITE_MODE:
-    logger.warning("Using SQLite for database")
-    OVERSEER.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///fediseer.db"
-else:
-    OVERSEER.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('POSTGRES_URI')
-    OVERSEER.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
-        "pool_size": 50,
-        "max_overflow": -1,
-    }
-OVERSEER.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(OVERSEER)
-db.init_app(OVERSEER)
 
-if not SQLITE_MODE:
-    with OVERSEER.app_context():
-        logger.debug("pool size = {}".format(db.engine.pool.size()))
-logger.init_ok("Fediseer Database", status="Started")
+
+def create_app():
+    OVERSEER = Flask(__name__)
+    OVERSEER.wsgi_app = ProxyFix(OVERSEER.wsgi_app, x_for=1)
+
+    if SQLITE_MODE:
+        logger.warning("Using SQLite for database")
+        OVERSEER.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///fediseer.db"
+    else:
+        OVERSEER.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('POSTGRES_URI')
+        OVERSEER.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
+            "pool_size": 50,
+            "max_overflow": -1,
+        }
+    OVERSEER.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(OVERSEER)
+    if not SQLITE_MODE:
+        with OVERSEER.app_context():
+            logger.debug("pool size = {}".format(db.engine.pool.size()))
+    logger.init_ok("Fediseer Database", status="Started")
+
+db = SQLAlchemy()
+OVERSEER = create_app()
 
 # Allow local workstation run
 if cache is None:
